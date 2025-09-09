b82ac198d352eced07e350422f9647fce6db09a6
RVPic <herve.picard@orange.com>
2025-09-09 09:47:12 -0400

    followersCountBis

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -6,6 +6,13 @@ function increaseCount() {
   checkCountValue(); // Vérifier la valeur du count et afficher les messages
 }
 
+function reinitCount() {
+    count = 0; // Reinitialize count to 0
+    displayCount(); // Affiche le compte
+    checkCountValue(); // Vérifier la valeur du count et afficher les messages
+    alertReinitCount(); // Le compteur a été réinitialisé
+  }
+
 function displayCount() {
 document.getElementById('countDisplay').innerHTML=count; // Affiche le compte dans l'HTML
 }
@@ -16,4 +23,8 @@ function checkCountValue() {
   } else if (count === 20) {
     alert("Votre publication Instagram a gagné 20 abonnés ! Continuez comme ça !");
   }
+}
+
+function alertReinitCount() {
+    alert("Attention, le compteur de vos followers a été réinitialisé !");
 }
